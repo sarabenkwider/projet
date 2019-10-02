375ee0ed0b48ea0b2e7a5256a0849a7e9d79fcac
sarabenkwider <sarabenkwider@gmail.com>
2019-10-02 11:36:20 +0200

    commit 1

---
+++ src/main/java/fr/altran/projet1test/Projet1TestApplication.java
@@ -0,0 +1,13 @@
+package fr.altran.projet1test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet1TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet1TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/altran/projet1test/Projet1TestApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.altran.projet1test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Projet1TestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
